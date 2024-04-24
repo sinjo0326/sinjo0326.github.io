ffdde4f6c1ff2f26b143307c2555b980cd676463
sinjo0326 <o.shinjyo@gmail.com>
2024-04-24 22:26:09 +0900

    6*5などに対応

--- scripts/main.js
+++ scripts/main.js
@@ -26,18 +26,19 @@ function doSekigae() {
     hideUnnecessaryCells();
 
     // 2次元配列の要素数取得・設定
-    var dimension = Number(document.getElementById('dimension').value);
-	// 6*6の人数分に足りない分は欠席者として扱う(例：3次元の場合は36 - 3 * 3 = 27人は欠席扱い)
-    var intAbsenteeNum = 36 - (dimension * dimension);
+    const { firstValue: rowCount, secondValue: columnCount } = splitValues(document.getElementById('dimension').value);
 
-    var arrayStudent = new Array(dimension)
-    for(let y = 0; y < dimension; y++) {
-    	arrayStudent[y] = new Array(dimension).fill(y);
+	// 6*6の人数分に足りない分は欠席者として扱う(例：3次元の場合は36 - 3 * 3 = 27人は欠席扱い)
+    var intAbsenteeNum = 36 - (rowCount * columnCount);
+
+    var arrayStudent = new Array(rowCount)
+    for(let y = 0; y < rowCount; y++) {
+    	arrayStudent[y] = new Array(rowCount).fill(y);
     }
 
     // 出欠のリストを読み込みつつ設定
-    for (var i = 0; i < dimension; i++) {
-    	for (var j = 0; j < dimension; j++) {
+    for (var i = 0; i < rowCount; i++) {
+    	for (var j = 0; j < columnCount; j++) {
 			var id = "s" + String(i) + String(j);
 			var selectedIndex = document.getElementById(id).selectedIndex;
     		if (selectedIndex == 1) {
@@ -66,8 +67,8 @@ function doSekigae() {
 	targetArray = Shuffle(targetArray);
 	targetIndex = 0;
 
-    for (var i = 0; i < dimension; i++) {
-    	for (var j = 0; j < dimension; j++) {
+    for (var i = 0; i < rowCount; i++) {
+    	for (var j = 0; j < columnCount; j++) {
 			var id = String(i) + String(j);
 			var studentValue = arrayStudent[i][j];
     		if (studentValue != "欠") {
@@ -111,14 +112,15 @@ function hideUnnecessaryCells() {
     	}
     }
 
-    var dimension = Number(document.getElementById('dimension').value);
+    // 2次元配列の要素数取得・設定
+    const { firstValue: rowCount, secondValue: columnCount } = splitValues(document.getElementById('dimension').value);
 
     // 不要なセルは非表示にする
     for (var i = 0; i < MAX_DIMENSION; i++) {
     	for (var j = 0; j < MAX_DIMENSION; j++) {
 			var id = String(i) + String(j);
 			var tid = "t" + String(i) + String(j);
-    		if ((i < dimension) && (j < dimension)) {
+    		if ((i < rowCount) && (j < columnCount)) {
         		if (document.getElementById(id).classList.contains("nodisplay")) {
         			document.getElementById(id).classList.remove("nodisplay");
         		}
@@ -133,121 +135,19 @@ function hideUnnecessaryCells() {
     }
 }
 
-//'*******************************
-//' 座席グループ分けツール
-//' ver           0.1.0
-//' last update   2016/04/03
-//' auther        o.shinjo
-//' history       2016/04/03 新規作成
-//'
-//'*******************************
-//Sub グループ分け(ByVal groupUnitNum As Integer)
-//    '生徒用の表をクリア
-//    Range(Cells(4, 2), Cells(9, 7)).ClearContents
-//
-//    '2人組用、3人組用、4人組用の固定値リストを宣言する。36要素。AABBCC・・・
-//    Dim LIST_2PAIR As Variant
-//    Dim LIST_3PAIR As Variant
-//    Dim LIST_4PAIR As Variant
-//
-//    LIST_2PAIR = Array("A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J", "K", "K", "L", "L", "M", "M", "N", "N", "O", "O", "P", "P", "Q", "Q", "R", "R")
-//    LIST_3PAIR = Array("A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "D", "E", "E", "E", "F", "F", "F", "G", "G", "G", "H", "H", "H", "I", "I", "I", "J", "J", "J", "K", "K", "K", "L", "L", "L")
-//    LIST_4PAIR = Array("A", "A", "A", "A", "B", "B", "B", "B", "C", "C", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "F", "F", "F", "F", "G", "G", "G", "G", "H", "H", "H", "H", "I", "I", "I", "I")
-//
-//    'シート名の先頭をとって2次元配列の要素数にするnew List(n-1,n-1)
-//    Dim strSheetName As String
-//    strSheetName = ActiveSheet.Name
-//    Dim dimension As Integer
-//    dimension = CInt(Left(strSheetName, 1))
-//
-//    Dim arrayStudent() As String
-//    ReDim arrayStudent(dimension - 1, dimension - 1)
-//
-//    '出欠のリストを反転させつつ2次元配列化
-//    '表示用の表の先頭B4列と欠席入力用の先頭B14列は固定
-//    'B14列の値(Cell(14, 2))がMapの最大(dimension - 1, dimension - 1)に入る
-//    Dim arrayStudentI As Integer
-//    Dim arrayStudentJ As Integer
-//    arrayStudentI = dimension - 1
-//    arrayStudentJ = dimension - 1
-//
-//    Dim intAbsenteeNum As Integer
-//    intAbsenteeNum = 0
-//
-//    For i = 14 To 14 + dimension
-//        For j = 2 To 2 + dimension
-//            If Cells(i, j).Value = "欠" Then
-//                '欠席の生徒の場所に「欠」
-//                '欠席の人数と位置を確認
-//                arrayStudent(arrayStudentI, arrayStudentJ) = "欠"
-//                intAbsenteeNum = intAbsenteeNum + 1
-//            End If
-//            arrayStudentJ = arrayStudentJ - 1
-//        Next j
-//        arrayStudentJ = dimension - 1
-//        arrayStudentI = arrayStudentI - 1
-//    Next i
-//
-//    '列数の２乗 - 欠席者のリストを作成
-//    Dim arrayInsert() As String
-//    ReDim arrayInsert(dimension * dimension - intAbsenteeNum - 1)
-//
-//    '2人組用、3人組用、4人組用の固定値リストから値をコピーし、できたリストの要素をシャッフル
-//    Dim arrayTarget As Variant
-//    Dim arrayCopy As Variant
-//    ReDim arrayCopy(dimension * dimension - intAbsenteeNum - 1)
-//    If groupUnitNum = 2 Then
-//        arrayTarget = LIST_2PAIR
-//    ElseIf groupUnitNum = 3 Then
-//        arrayTarget = LIST_3PAIR
-//    ElseIf groupUnitNum = 4 Then
-//        arrayTarget = LIST_4PAIR
-//    End If
-//
-//    For i = 0 To dimension * dimension - intAbsenteeNum - 1
-//        arrayCopy(i) = arrayTarget(i)
-//    Next i
-//    arrayCopy = Shuffle(arrayCopy)
-//
-//    For i = 0 To dimension * dimension - intAbsenteeNum - 1
-//        arrayInsert(i) = arrayCopy(i)
-//    Next i
-//
-//    '生徒目線のMapを作成new Map(n,n)
-//    '生徒目線のMapに欠席者の値を入れる（反転して）。値は空文字。
-//    '空いてるMapにシャッフルした値を入れる
-//    'Mapの値を書き出し
-//    Dim arrayInsertIndex As Integer
-//    arrayInsertIndex = 0
-//    arrayStudentI = 0
-//    arrayStudentJ = 0
-//    For i = 4 To 4 + dimension - 1
-//        For j = 2 To 2 + dimension - 1
-//            If arrayStudent(arrayStudentI, arrayStudentJ) = "欠" Then
-//                '欠席の生徒の場所に「欠」
-//                '欠席の人数と位置を確認
-//                Cells(i, j).Value = ""
-//            Else
-//                Cells(i, j).Value = arrayInsert(arrayInsertIndex)
-//                arrayInsertIndex = arrayInsertIndex + 1
-//            End If
-//            arrayStudentJ = arrayStudentJ + 1
-//        Next j
-//        arrayStudentJ = 0
-//        arrayStudentI = arrayStudentI + 1
-//    Next i
-//End Sub
-//
-//' 配列をシャッフルする関数
-//Private Function Shuffle(list)
-//    For i = 0 To UBound(list)
-//        Randomize
-//        rn = Int(UBound(list) * Rnd)
-//        tmp = list(i)
-//        list(i) = list(rn)
-//        list(rn) = tmp
-//    Next
-//    Shuffle = list
-//End Function
-//
+function splitValues(value) {
+    let firstValue, secondValue;
 
+    if (value.includes('_')) {
+        const parts = value.split('_').map(Number);
+        firstValue = parts[0];
+        secondValue = parts[1];
+    } else {
+        // _が含まれていない場合は、両方の値を同じ数としてセットする
+        const singleValue = Number(value);
+        firstValue = singleValue;
+        secondValue = singleValue;
+    }
+
+    return { firstValue, secondValue };
+}
